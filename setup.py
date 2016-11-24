fe45088049cf51a938de19c49a96b06ae7110b64
Anp-tomsk <anp.tomsk@gmail.com>
2016-11-24 22:07:51 +0600

    Add setup py and requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='event-sourcing',
+    version='0.1.0',
+    packages=['utils', 'actions', 'queries', 'handlers'],
+    url='',
+    license='',
+    author='anp',
+    author_email='',
+    description=''
+)
